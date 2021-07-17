456a1d38a16ac81b5fdcf2cfbb094dfde719457c
Santiago Andres Blanco Marquez <sablancom@unal.edu.co>
2021-07-16 20:55:45 -0500

    .NET console project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ease_challenge_repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
